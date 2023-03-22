c68685fb9a4cf22f94025a72018d92d46621271f
Adam Li <adam2392@gmail.com>
2023-03-22 14:40:45 -0400

    Fix docstring for oblique trees
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- sklearn/ensemble/_forest.py
+++ sklearn/ensemble/_forest.py
@@ -3054,7 +3054,7 @@ class ObliqueRandomForestClassifier(ForestClassifier):
 
     Attributes
     ----------
-    base_estimator_ : ObliqueDecisionTreeClassifier
+    estimator_ : :class:`~sklearn.tree.DecisionTreeClassifier`
         The child estimator template used to create the collection of fitted
         sub-estimators.
 
@@ -3069,23 +3069,12 @@ class ObliqueRandomForestClassifier(ForestClassifier):
         The number of classes (single output problem), or a list containing the
         number of classes for each output (multi-output problem).
 
-    n_features_ : int
-        The number of features when ``fit`` is performed.
-
-        .. deprecated:: 1.0
-            Attribute `n_features_` was deprecated in version 1.0 and will be
-            removed in 1.2. Use `n_features_in_` instead.
-
     n_features_in_ : int
         Number of features seen during :term:`fit`.
-
-        .. versionadded:: 0.24
 
     feature_names_in_ : ndarray of shape (`n_features_in_`,)
         Names of features seen during :term:`fit`. Defined only when `X`
         has feature names that are all strings.
-
-        .. versionadded:: 1.0
 
     n_outputs_ : int
         The number of outputs when ``fit`` is performed.
