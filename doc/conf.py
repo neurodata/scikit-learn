7d52ef0024eda2dc866ce195b9994cf25dc5e830
Adam Li <adam2392@gmail.com>
2023-03-27 11:03:35 -0700

    Add all changelog to the README
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- doc/conf.py
+++ doc/conf.py
@@ -211,7 +211,7 @@ html_static_path = ["images"]
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
-html_additional_pages = {"sklearn_index": "index.html"}
+html_additional_pages = {"index": "index.html"}
 
 # If false, no module index is generated.
 html_domain_indices = False
