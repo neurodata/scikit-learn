0189209db3223edd23954f7b664179bc8efd5d2d
Haoyin Xu <haoyinxu@gmail.com>
2026-04-10 19:31:07 -0400

    FIX correct parameter

--- sklearn/ensemble/_forest.py
+++ sklearn/ensemble/_forest.py
@@ -1275,7 +1275,7 @@ class ForestClassifier(ClassifierMixin, BaseForest, metaclass=ABCMeta):
             multi_output=True,
             accept_sparse="csc",
             dtype=DTYPE,
-            force_all_finite=False,
+            ensure_all_finite=False,
             reset=first_call,
         )
 
